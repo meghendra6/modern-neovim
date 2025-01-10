973ceb4a7f31e06bbebcee532ec7b593989e1bc2
Scott Lee <scott.lee@sapeon.com>
2025-01-10 18:05:53 +0900

    disable typescript

--- lua/plugins/extras/lang/typescript.lua
+++ lua/plugins/extras/lang/typescript.lua
@@ -15,15 +15,15 @@ return {
       },
       setup = {
         ts_ls = function(_, opts)
-          local lsp_utils = require "plugins.lsp.utils"
-          lsp_utils.on_attach(function(client, buffer)
-            if client.name == "ts_ls" then
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-              vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-            end
-          end)
-          require("typescript").setup { server = opts }
+          --          local lsp_utils = require "plugins.lsp.utils"
+          --          lsp_utils.on_attach(function(client, buffer)
+          --            if client.name == "ts_ls" then
+          --              -- stylua: ignore
+          --              vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          --              vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+          --            end
+          --          end)
+         -- require("typescript").setup { server = opts }
           return true
         end,
       },
